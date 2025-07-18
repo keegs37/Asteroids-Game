d072a07adeb7f6091a406d78a2e715680120b8b4
keegs37 <keegs37>
2025-07-18 13:44:24 -0700

    Added player to the screen with the abilty to rotate left and right

--- main.py
+++ main.py
@@ -1,19 +1,29 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    game_clock = pygame.time.Clock()
+    dt = 0 
+    player_1 = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill(000)
+        screen.fill((0,0,0))
+        player_1.update(dt)
+        player_1.draw(screen)
         pygame.display.flip()
+        
+        dt = game_clock.tick(60) / 1000
+
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
-
+    
+    
 
 if __name__ == "__main__":
     main()
