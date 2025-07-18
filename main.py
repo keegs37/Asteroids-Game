780b9d678ffb4f768a5bd916403a9d6b9aeb0416
keegs37 <keegs37>
2025-07-18 15:06:51 -0700

    Added the Player class to the updateable and drawable groups

--- main.py
+++ main.py
@@ -3,24 +3,42 @@ from constants import *
 from player import *
 
 def main():
+
     pygame.init()
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
     game_clock = pygame.time.Clock()
+
     dt = 0 
+
+    updateable = pygame.sprite.Group()
+
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updateable, drawable)
+
     player_1 = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0,0,0))
-        player_1.update(dt)
-        player_1.draw(screen)
+
+        updateable.update(dt)
+
+        for entity in drawable:
+            entity.draw(screen)
+
         pygame.display.flip()
         
         dt = game_clock.tick(60) / 1000
 
     print("Starting Asteroids!")
+
     print(f"Screen width: {SCREEN_WIDTH}")
+
     print(f"Screen height: {SCREEN_HEIGHT}")
     
     
